0bad759eb09aa65de433cbbba548a58f081319f1
JeanYSu <ysu@g.hardvard.edu>
2015-09-15 21:21:07 -0700

    add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<title>CSCI E-15 P2 Home Page</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+
+	<?php
+	echo 'Porject #: Project 2 <br>';
+	echo 'Project Member: Jean<br>';
+	echo 'Server Name: ';
+	echo $_SERVER['SERVER_NAME'];
+	?>
+	<br>
+	<br>
+	<br>
+	<h1>Under Construction</h1>
+</body>
+</html>
